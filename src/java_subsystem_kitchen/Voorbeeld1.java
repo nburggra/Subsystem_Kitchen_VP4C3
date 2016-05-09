023d45f24100e5df8c39788b8bf9ed333cd62892
Rubens van Eijden <rubensvaneijden@hotmail.com>
2016-05-09 19:46:01 +0200

    V0.1.0

--- src/java_subsystem_kitchen/Voorbeeld1.java
+++ src/java_subsystem_kitchen/Voorbeeld1.java
@@ -1,5 +1,9 @@
 package java_subsystem_kitchen;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +14,35 @@ package java_subsystem_kitchen;
  *
  * @author NickB
  */
-public class Voorbeeld1 {
+public class JavaGUI extends JFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   JFrame frame = new JavaGUI();
+   frame.setSize( 400, 200);
+   frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+   frame.setTitle( "Java GUI");
+   frame.setContentPane( new FlowLayoutpaneel() );
+   frame.setVisible(true);
+    }
+}
+
+class FlowLayoutpaneel extends JPanel {
+    private JTextField t1, t2, t3, t4, t5;
     
+    public FlowLayoutpaneel() {
+        t1 = new JTextField( "een", 20 );
+        t2 = new JTextField( "twee", 20 );
+        t3 = new JTextField( "drie", 20 );
+        t4 = new JTextField( "vier", 20 );
+        t5 = new JTextField( "vijf", 20 );
+        add(t1);
+        add(t2);
+        add(t3);
+        add(t4);
+        add(t5);
+        
+    }
 }
